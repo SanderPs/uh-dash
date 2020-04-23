3ab431815fa2a7d42b87bb0ab38713735ecb29f3
SanderPs <dev@sanderpeschier.nl>
2020-04-23 14:05:25 +0200

    show radical selected; dont show rest

--- src/components/uh-dash-radicals/uh-dash-radicals.tsx
+++ src/components/uh-dash-radicals/uh-dash-radicals.tsx
@@ -1,4 +1,4 @@
-import { Component, Event, EventEmitter, h } from '@stencil/core';
+import { Component, Event, EventEmitter, State, h } from '@stencil/core';
 import { uhRadicals, IUnihanRadicals } from './uh-radicals';
 
 @Component({
@@ -8,8 +8,10 @@ import { uhRadicals, IUnihanRadicals } from './uh-radicals';
 export class UhDashRadicals {
 
   buttons = [];
+  selectedButton: string;
   theList: IUnihanRadicals;
-  selectedRadical: number = null;
+  @State() selectedRadical: number = null;
+
   @Event({
     eventName: 'radSelected'
   }) onRadSelected: EventEmitter;
@@ -21,26 +23,35 @@ export class UhDashRadicals {
   render() {
     this.buttons = []
 
-    for (const [radId, rad] of Object.entries(this.theList)) {
-      if (rad.newline) {
-        this.buttons.push(<div class="rad-break"></div>)
-      }
-      if (rad.strokeHdr) {
+    if (this.selectedRadical) {
+      this.selectedButton = <div class="rad" onClick={() => this.unselectRad()}>
+          {String.fromCharCode(this.theList[this.selectedRadical].trCJK)}
+        </div>;
+
+    } else {
+      this.selectedButton = '';
+      for (const [radId, rad] of Object.entries(this.theList)) {
+        if (rad.newline) {
+          this.buttons.push(<div class="rad-break"></div>)
+        }
+        if (rad.strokeHdr) {
+          this.buttons.push(
+            <div key={'hdr-' + radId} class="rad hdr">
+              {rad.strokeHdr}
+            </div>
+          )
+        }
         this.buttons.push(
-          <div key={'hdr-' + radId} class="rad hdr">
-            {rad.strokeHdr}
+          <div key={radId} title={radId.slice(0, -1)} class="rad" onClick={() => this.selectRad(parseInt(radId, 10))}>
+            {String.fromCharCode(rad.trCJK)}
           </div>
         )
       }
-      this.buttons.push(
-        <div key={radId} title={radId.slice(0, -1)} class="rad" onClick={() => this.selectRad(parseInt(radId,10))}>
-          {String.fromCharCode(rad.trCJK)}
-        </div>
-      )
     }
-
     return <div>
-      <div class="radicals-header">header</div>
+      <div class="radicals-header">
+        {this.selectedButton}
+      </div>
       <div class="radicals-wrapper">{this.buttons}</div>
     </div>
   }
@@ -49,5 +60,9 @@ export class UhDashRadicals {
     this.selectedRadical = (this.selectedRadical === radId) ? null : radId;
 
     this.onRadSelected.emit(this.selectedRadical);
+  }
+
+  unselectRad() {
+    this.selectedRadical = null;
   }
 }
